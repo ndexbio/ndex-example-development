d577c2656a059d5fe7d2530095bfa72abde72e52
Dexter Pratt <dexterpratt.bio@gmail.com>
2018-01-24 11:06:44 -0800

    Signor pipeline and tutorial normalization

--- resources/tutorial_utils.py
+++ resources/tutorial_utils.py
@@ -1,21 +1,17 @@
 from os.path import isfile, expanduser
-import sys
-sys.path.append("/Users/dexter/Projects/ndex2-client")
 import ndex2
 import json
 nicecx = ndex2.NiceCXNetwork
 Ndex2 = ndex2.Ndex2
 
 
-def load_tutorial_config(connection_name=None):
+def load_tutorial_config(connection_name):
     username = None
     password = None
-    server = "public.ndexbio.org"
-    my_ndex2_client = None
     config_file = expanduser("~/ndex_tutorial_config.json")
-    save_tutorial_networks_to_my_account = True
+    server = "http://public.ndexbio.org"
 
-    if(isfile(config_file)):
+    if isfile(config_file):
         file = open(config_file, "r")
         config = json.load(file)
         file.close()
@@ -28,30 +24,11 @@ def load_tutorial_config(connection_name=None):
                         server = connection.get("server")
                     username = connection.get("username")
                     password = connection.get("password")
-
             else:
                 print("Error: " + str(connection_name) + " does not define both username and password")
-
         else:
             print("Error: " + config_file + " does not define any connections")
     else:
         print("Error: " + config_file + " was not found")
 
-        #
-    try:
-        # Test the connection
-        my_ndex=Ndex2(server, username, password)
-        tutorial_private_network_uuid = ""
-        private_network_cx = my_ndex.
-        my_ndex.update_status()
-        networks = my_ndex.status.get("networkCount")
-        users = my_ndex.status.get("userCount")
-        groups = my_ndex.status.get("groupCount")
-        print("my_ndex client: %s networks, %s users, %s groups" % (networks, users, groups))
-        # TODO - check that user can access private account details
-
-    except Exception as inst:
-        print("Could not access account %s with password %s" % (username, password))
-        print(inst.args)
-
-    return (server, username, password)
+    return server, username, password
